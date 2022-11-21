83e4bf6cbce457acb56dc5a495fe5c715ccad689
Foxicution <foxicution@gmail.com>
2022-11-21 16:58:53 +0200

    Updated graph font option to be visible both in dark and light mode

---
+++ streamlit_components/graph_visualizer/frontend/build/precache-manifest.12fa06fa81cf1ce128be61a33fd013a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7259b4a55896b90ca73e10f150857e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7521929ea4ccc00ce564",
+    "url": "./static/js/2.211ffd16.chunk.js"
+  },
+  {
+    "revision": "d2e11bed97bb9d9d0b1819932469a067",
+    "url": "./static/js/2.211ffd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b11f3bf74978e225e3f",
+    "url": "./static/js/main.8a7ab0e0.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
