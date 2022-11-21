89b39a8b3d0b96fcdd4a02d2d655ffe83e02e7ba
Foxicution <foxicution@gmail.com>
2022-11-21 14:54:11 +0200

    Updated frontend component

---
+++ streamlit_components/graph_visualizer/frontend/build/precache-manifest.4543b1fae28a0d0e98211e4a255ff00c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d56a2949fb539d68bcd0f7a1473e1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85f8df4900d9c2b80f81",
+    "url": "./static/js/2.3173932f.chunk.js"
+  },
+  {
+    "revision": "d2e11bed97bb9d9d0b1819932469a067",
+    "url": "./static/js/2.3173932f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10fbf1aab7b01f281c4",
+    "url": "./static/js/main.4300678b.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
