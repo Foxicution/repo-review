e9e190f8c76e1a6ea642c1605301f4f4a2406658
Foxicution <foxicution@gmail.com>
2022-12-10 20:11:02 +0200

    Removed types module and created generics

---
+++ src/generics.py
@@ -0,0 +1,17 @@
+from typing import TypeVar, Callable
+from option import Result, Ok, Err, Option, Some, NONE 
+
+T = TypeVar('T')
+
+def try_decorator(function: Callable[..., T]) -> Callable[..., Result[T, Exception]]:
+    """Decorator that wraps a function in a try/except block and returns a Result monad"""
+    def wrapper(*args, **kwargs) -> Result[T, Exception]:
+        try:
+            return Ok(function(*args, **kwargs))
+        except Exception as e:
+            return Err(e)
+    return wrapper
+
+def call_on_input(input, function: Callable[..., T]) -> Option[T]:
+    """Calls a function on an input if the input is not None and returns an Option monad"""
+    return Some(function(input)) if input else NONE
