9a5f41d667cae9c4a3965cb46ef21d725cfa6bf2
Foxicution <foxicution@gmail.com>
2022-12-10 20:59:56 +0200

    Added a basic UI to take input and display files

--- src/ui.py
+++ src/ui.py
@@ -1,1 +1,9 @@
-from src.files_from_github import get_files
+from files_from_github import get_files
+from streamlit import text_input, text
+from toolz.functoolz import pipe
+
+def main():
+    pipe(text_input('Input repository link'), get_files, list, text)
+
+if __name__ == '__main__':
+    main()
