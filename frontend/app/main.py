00b741551d18f5ad37e07c73201ef80468e8bd05
Foxicution <foxicution@gmail.com>
2022-12-13 19:05:03 +0200

    Fixed Jinja directory and added page.html

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="frontend/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
