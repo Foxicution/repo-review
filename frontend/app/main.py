55fbe1854f17091073eb77d021587667c9b7da17
Foxicution <foxicution@gmail.com>
2022-12-13 18:56:24 +0200

    Moved app folder to frontend for structurization

---
+++ frontend/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = {
+        "page": "Home page"
+    }
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
+    data = {
+        "page": page_name
+    }
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
