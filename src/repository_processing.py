50d5c05336988fca2d0d9124b724443df9461619
Foxicution <foxicution@gmail.com>
2023-01-07 18:06:24 +0200

    fixed str docs inside of the module

--- src/repository_processing.py
+++ src/repository_processing.py
@@ -8,14 +8,14 @@ from toolz.functoolz import pipe
 
 
 class File(BaseModel):
-    """Class that represents a file in a repository"""
+    """represents a file in a repository"""
 
     path: str
     content: str
 
 
 def random_string(string_length: int = 10) -> str:
-    """Function that generates a random string of fixed length"""
+    """generates a random string of fixed length"""
     from random import choice
     from string import ascii_lowercase
 
@@ -24,35 +24,35 @@ def random_string(string_length: int = 10) -> str:
 
 
 def to_url(string: str) -> str:
-    """Function that converts a string to a url using % encoding"""
+    """converts a string to a url using % encoding"""
     return quote(string, safe="")
 
 
 def from_url(url: str) -> str:
-    """Function that converts a url to a string using % decoding"""
+    """converts a url to a string using % decoding"""
     return unquote(url)
 
 
 def format_repo_url(repo_url: str) -> str:
-    """Function that removes the storage system domain from the repo url"""
+    """removes the storage system domain from the repo url"""
     return repo_url.split("github.com/")[-1]
 
 
 def pull_repo(repo_path) -> Repo:
-    """Function that pulls the latest changes from a repository in a local directory"""
+    """pulls the latest changes from a repository in a local directory"""
     repo = Repo(repo_path)
     repo.remote().pull()
     return repo
 
 
 def get_repo(repo_url: str) -> Repo:
-    """Function that clones a repository to a local directory"""
+    """clones a repository to a local directory"""
     repo_path = f"repos/{pipe(repo_url, format_repo_url, to_url)}"
     return pull_repo(repo_path) if exists(repo_path) else Repo.clone_from(repo_url, repo_path)
 
 
 def traverse_tree(tree: Tree, path: str = "") -> list[File]:
-    """Function that traverses a tree and returns the contents of each file"""
+    """traverses a tree and returns the contents of each file"""
     for blob in tree.blobs:
         yield File(path=f"{path}/{blob.name}", content=blob.data_stream.read())
     for sub_tree in tree.trees:
@@ -60,6 +60,7 @@ def traverse_tree(tree: Tree, path: str = "") -> list[File]:
 
 
 def repo_file_list(repo: Repo) -> list[File]:
+    """returns the contents of all files in a repository"""
     tree = repo.head.commit.tree
     return list(traverse_tree(tree))
 
